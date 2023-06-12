c630df58d80cc55d7160af6bd6c4732b77940950
Giovannicampo <giovanni.campo144@gmail.com>
2023-06-12 17:21:10 +0200

    implemented a 3x3invert method for matrix

--- lib/Matrix.cpp
+++ lib/Matrix.cpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdio.h>
+#include <math.h>
 
 class Matrix
 {
@@ -10,6 +11,41 @@ class Matrix
         unsigned int num_rows;
 
     public:
+
+        //--------------------------------------------------------------------------------
+        // ATTENZIONE -> determinante implementato solo per matrici con ordine minore <= 3
+        //--------------------------------------------------------------------------------
+        void determinante (double* det)
+        {
+            if(this->num_cols != this->num_rows){
+                printf("la matrice non è quadrata, non è possibile calcolarne il determinante\n");
+                return;
+            }
+
+            if(num_cols > 3){
+                printf("not implemented yet! sorry\n");
+            }
+
+            if(num_cols == 1){
+                *det = matrix[0][0];
+                return;
+            }
+
+            if(num_cols == 2){
+                *det = (matrix[0][0] * matrix[1][1]) -
+                       (matrix[1][0] * matrix[0][1]);
+                return;
+            }
+if(num_cols > 3){
+                printf("not implemented yet! sorry\n");
+            }
+            *det = (matrix[0][0] * matrix[1][1] * matrix[2][2]) +
+                   (matrix[0][1] * matrix[1][2] * matrix[2][0]) +
+                   (matrix[0][2] * matrix[1][0] * matrix[2][1]) -
+                   (matrix[2][0] * matrix[1][1] * matrix[0][2]) -
+                   (matrix[2][1] * matrix[1][2] * matrix[0][0]) -
+                   (matrix[2][2] * matrix[1][0] * matrix[0][1]);
+        }
 
         // matrix nxm
         Matrix (unsigned int _num_rows, unsigned int _num_cols, double _matrix[])
@@ -42,13 +78,17 @@ class Matrix
             }
         }
 
+        Matrix(const Matrix& m)
+            : num_cols{m.cols()}, num_rows{m.rows()}, matrix{m.getMatrix()}
+        {}
+
         void print ()
         {
             for(unsigned short i=0; i<num_rows; i++)
             {
                 for(unsigned short j=0; j<num_cols; j++)
                 {
-                    printf("%.3f ", matrix[i][j]);
+                    printf("%.5f ", matrix[i][j]);
                 }
                 printf("\n");
             }
@@ -137,7 +177,79 @@ class Matrix
             }
 
             return Matrix(_num_rows, _num_cols, temp_m);
+        } 
+
+        double cofattore3x3(unsigned int ix, unsigned int iy)
+        {
+            double temp_m[4];
+            unsigned int index = 0;
+
+            for(unsigned short i=0; i<this->num_rows; i++)
+            {
+                for(unsigned short j=0; j<this->num_cols; j++)
+                {
+                    if(i == ix || j == iy){
+                        continue;
+                    }
+                    temp_m[index] = matrix[i][j];
+                    index++;
+                }
+            }
+
+            Matrix M(2,2,temp_m);
+            double det;
+            M.determinante(&det);
+
+            // printf("cof (%d,%d): %f\n", ix, iy, (double) det * pow((-1),(ix+iy)));
+
+            return (double) det * pow((-1),(ix+iy));
         }
 
+        //--------------------------------------------------------
+        // ATTENZIONE -> Inverti implementata solo per matrici 3x3
+        //--------------------------------------------------------
+        void invert3x3 ()
+        {
+            // STEP 0 -> controllare se la matrice è quadrata
+            if(this->num_cols != this->num_rows){
+                printf("la matrice non è quadrata, quindi non è invertibile\n");
+                return;
+            }
+
+            if(num_cols > 3){
+                printf("not implemented yet! sorry\n");
+                return;
+            }
+
+            // STEP 1 -> controllare se il determinante della matrice è nullo
+            double det;
+            this->determinante(&det);
+            if(det == 0){
+                printf("il determinante è nullo, quindi non è invertibile\n");
+                return;
+            }
+
+            // STEP 2 -> se non è nullo, la matrice è invertibile
+            double inverted_matrix[this->num_cols * this->num_rows];
+            unsigned int index = 0;
+
+            for(unsigned short i=0; i<num_rows; i++)
+            {
+                for(unsigned short j=0; j<num_cols; j++)
+                {
+                    inverted_matrix[index] = this->cofattore3x3(i,j);
+                    index++;
+                }
+            }
+
+            for(unsigned short i=0; i<num_rows; i++){
+                for(unsigned short j=0; j<num_cols; j++)
+                {
+                    matrix[i][j] = inverted_matrix[j + num_cols*i];
+                }
+            }
+            this->transpose();
+            *(this) = (*this)*(1/det);
+        }
 
 };
--- main.cpp
+++ main.cpp
@@ -3,34 +3,46 @@
 
 int main () 
 {
-    double identity[] = { 1.0, 0.0, 0.0,
-                          0.0, 1.0, 0.0,
-                          0.0, 0.0, 1.0};
-    Matrix* m = new Matrix(3,3,identity);
-    Matrix* i = new Matrix(3);
-    // m->print();
-    // i->print();
+    // double identity[] = { 1.0, 0.0, 0.0,
+    //                       0.0, 1.0, 0.0,
+    //                       0.0, 0.0, 1.0};
+    // Matrix* m = new Matrix(3,3,identity);
+    // Matrix* i = new Matrix(3);
+    // // m->print();
+    // // i->print();
 
-    double m1[] = {1.0, 2.0, 3.0, 4.0,
-                   5.0, 6.0, 7.0, 8.0};
-    Matrix* M1 = new Matrix(2,4,m1);
-    // M1->print();
-    // M1->transpose();
-    // M1->print();
+    // double m1[] = {1.0, 2.0, 3.0, 4.0,
+    //                5.0, 6.0, 7.0, 8.0};
+    // Matrix* M1 = new Matrix(2,4,m1);
+    // // M1->print();
+    // // M1->transpose();
+    // // M1->print();
 
-    double m2[] = {1.0, 0.0, 2.0,
-                   0.0, 3.0, -1.0};
-    double m3[] = {4.0, 1.0,
-                   -2.0, 2.0,
-                   0.0, 3.0};
+    // double m2[] = {1.0, 0.0, 2.0,
+    //                0.0, 3.0, -1.0};
+    // double m3[] = {4.0, 1.0,
+    //                -2.0, 2.0,
+    //                0.0, 3.0};
     
-    Matrix M2(2,3,m2);
-    Matrix M3(3,2,m3);
-    Matrix M4 = M2*M3;
-    // M4.print();
+    // Matrix M2(2,3,m2);
+    // Matrix M3(3,2,m3);
+    // Matrix M4 = M2*M3;
+    // // M4.print();
 
-    Matrix M5 = (*i)*5.5;
-    // M5.print();
+    // Matrix M5 = (*i)*5.5;
+    // // M5.print();
+
+    // double m6[] = {1.0, 0.0, 1.0,
+    //                0.0, 2.0, 0.0,
+    //                3.0, 2.0, 0.0};
+    // Matrix M6(3,3,m6);
+    // double det_m6 = 0;
+    // M6.determinante(&det_m6);
+    // printf("determinante di M6: %f", det_m6);
+    // printf("\n");
+
+    // M6.invert3x3();
+    // M6.print();
 
     KalmanOdometry ko(270.0);
 }
