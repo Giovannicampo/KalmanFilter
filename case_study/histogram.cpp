17dba879b4c372fd8775afa9cc9788cfd6834544
Giovannicampo <giovanni.campo144@gmail.com>
2023-07-11 10:02:59 +0200

    adding new plot for 1.5 m and corrections

--- case_study/histogram.cpp
+++ case_study/histogram.cpp
@@ -6,13 +6,15 @@
 
 #define LEFT_DIM 174
 #define RIGHT_DIM 175
+#define LEFT_1_5 111
 
 int main()
 {
     std::map<int, int> hist_measure_left{};
     std::map<int, int> hist_measure_right{};
+    std::map<int, int> hist_measure_left_1_5{};
 
-    FILE* fp = fopen("data/tot_left_encoder.txt", "r");
+    FILE* fp = fopen("data/first_data/tot_left_encoder.txt", "r");
     int temp_value = 0;
 
     for(unsigned short i=0; i<LEFT_DIM; i++)
@@ -24,11 +26,20 @@ int main()
 
     fclose(fp);
 
-    fp = fopen("data/tot_right_encoder.txt", "r");
+    fp = fopen("data/first_data/tot_right_encoder.txt", "r");
     for(unsigned short i=0; i<RIGHT_DIM; i++)
     {
         fscanf(fp, "%d", &temp_value);
         ++hist_measure_right[(int)temp_value];
+    }
+
+    fclose(fp);
+
+    fp = fopen("data/1_5m_measures_left.txt", "r");
+    for(unsigned short i=0; i<LEFT_1_5; i++)
+    {
+        fscanf(fp, "%d", &temp_value);
+        ++hist_measure_left_1_5[(int)temp_value];
     }
 
     fclose(fp);
@@ -45,6 +56,13 @@ int main()
         std::cout << std::setw(2)
                   << p.first << ' ' << std::string(p.second*3, '*') << '\n';
     }
+    printf("\n");
+
+    printf("[Case_study] 1.5 M LEFT ENCODER HISTOGRAM\n");
+    for(auto p : hist_measure_left_1_5) {
+        std::cout << std::setw(2)
+                  << p.first << ' ' << std::string(p.second*3, '*') << '\n';
+    }
 
     printf("\n");
     printf("[Case_study] LEFT ENCODER DATA\n");
@@ -58,6 +76,14 @@ int main()
     printf("[Case_study] RIGHT ENCODER DATA\n");
 
     for(auto p : hist_measure_right) {
+        std::cout << std::setw(2)
+                  << p.first << ' ' << p.second << '\n';
+    }
+
+    printf("\n");
+    printf("[Case_study] 1.5 M LEFT ENCODER DATA\n");
+
+    for(auto p : hist_measure_left_1_5) {
         std::cout << std::setw(2)
                   << p.first << ' ' << p.second << '\n';
     }
