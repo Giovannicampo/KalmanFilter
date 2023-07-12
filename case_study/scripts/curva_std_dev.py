872db3ef325d4cd35928e367cabd39df517154fa
Giovannicampo <giovanni.campo144@gmail.com>
2023-07-12 11:16:32 +0200

    fix repo

---
+++ case_study/scripts/curva_std_dev.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x = [0, 75, 150, 300]
+y = [0, 14.28, 21.31, 25.68]
+
+plt.plot(x, y, marker='o')
+plt.xlabel('Distanza (cm)')
+plt.ylabel('Std_Dev')
+plt.title('Grafico della curva')
+plt.grid(True)
+plt.show()
