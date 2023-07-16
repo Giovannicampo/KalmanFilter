91b31851042c0e37af8c2f34a0daec952099615b
Giovannicampo <giovanni.campo144@gmail.com>
2023-07-16 16:06:36 +0200

    update ma cose da fare

---
+++ case_study/data/raw_data/histogram.cpp
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <random>
+
+#define DIM 111
+
+int main()
+{
+    // std::map<double, int> hist_measure_0_6{};
+    // std::map<int, double> hist_measure_0_75{};
+    // std::map<int, double> hist_measure_1_5{};
+    // std::map<int, double> hist_measure_3_0{};
+    // std::map<int, double> hist_measure_4_5{};
+    std::map<int, int> hist_measure_tof_0_2{};
+
+    // FILE* fp = fopen("0_6m.txt", "r");
+    int temp_value = 0;
+    // double temp_d_value = 0.0;
+
+    // for(unsigned short i=0; i<DIM; i++)
+    // {
+    //     fscanf(fp, "%lf", &temp_d_value);
+    //     ++hist_measure_0_6[(double)temp_d_value];
+    // }
+    // temp_d_value = 0.0;
+
+    // fclose(fp);
+
+    // fp = fopen("0_75m.txt", "r");
+    // for(unsigned short i=0; i<DIM; i++)
+    // {
+    //     fscanf(fp, "%lf", &temp_value);
+    //     ++hist_measure_0_75[(double)temp_value];
+    // }
+
+    // fclose(fp);
+
+    // fp = fopen("1_5m.txt", "r");
+    // for(unsigned short i=0; i<DIM; i++)
+    // {
+    //     fscanf(fp, "%lf", &temp_value);
+    //     ++hist_measure_1_5[(double)temp_value];
+    // }
+
+    // fclose(fp);
+
+    // fp = fopen("3_0m.txt", "r");
+    // for(unsigned short i=0; i<DIM; i++)
+    // {
+    //     fscanf(fp, "%lf", &temp_value);
+    //     ++hist_measure_3_0[(double)temp_value];
+    // }
+
+    // fclose(fp);
+
+    // fp = fopen("4_5m.txt", "r");
+    // for(unsigned short i=0; i<DIM; i++)
+    // {
+    //     fscanf(fp, "%lf", &temp_value);
+    //     ++hist_measure_4_5[(double)temp_value];
+    // }
+
+    // fclose(fp);
+
+    FILE* fp = fopen("tof_0_2m.txt", "r");
+    for(unsigned short i=0; i<DIM+1; i++)
+    {
+        fscanf(fp, "%d", &temp_value);
+        ++hist_measure_tof_0_2[(int)temp_value];
+    }
+
+    fclose(fp);
+
+    // printf("[Case_study] 0.6 m HISTOGRAM\n");
+    // for(auto p : hist_measure_0_6) {
+    //     std::cout << std::setw(2)
+    //               << p.first << ' ' << std::string((int)p.second*3, '*') << '\n';
+    // }
+    // printf("\n");
+
+    // printf("[Case_study] 0.6 m DATA\n");
+    // for(auto p : hist_measure_0_6) {
+    //     std::cout << std::setw(2)
+    //               << p.first << ' ' << p.second << '\n';
+    // }
+    // printf("\n");
+
+    // printf("[Case_study] 0.75 m HISTOGRAM\n");
+    // for(auto p : hist_measure_0_75) {
+    //     std::cout << std::setw(2)
+    //               << p.first << ' ' << std::string(p.second*3, '*') << '\n';
+    // }
+    // printf("\n");
+
+    // printf("[Case_study] 0.75 m DATA\n");
+    // for(auto p : hist_measure_0_75) {
+    //     std::cout << std::setw(2)
+    //               << p.first << ' ' << p.second << '\n';
+    // }
+    // printf("\n");
+
+    // printf("[Case_study] 1.5 m HISTOGRAM\n");
+    // for(auto p : hist_measure_1_5) {
+    //     std::cout << std::setw(2)
+    //               << p.first << ' ' << std::string(p.second*3, '*') << '\n';
+    // }
+    // printf("\n");
+
+    // printf("[Case_study] 1.5 m DATA\n");
+    // for(auto p : hist_measure_1_5) {
+    //     std::cout << std::setw(2)
+    //               << p.first << ' ' << p.second << '\n';
+    // }
+    // printf("\n");
+
+    // printf("[Case_study] 3.0 m HISTOGRAM\n");
+    // for(auto p : hist_measure_3_0) {
+    //     std::cout << std::setw(2)
+    //               << p.first << ' ' << std::string(p.second*3, '*') << '\n';
+    // }
+    // printf("\n");
+
+    // printf("[Case_study] 3.0 m DATA\n");
+    // for(auto p : hist_measure_3_0) {
+    //     std::cout << std::setw(2)
+    //               << p.first << ' ' << p.second << '\n';
+    // }
+    // printf("\n");
+
+    // printf("[Case_study] 4.5 m HISTOGRAM\n");
+    // for(auto p : hist_measure_4_5) {
+    //     std::cout << std::setw(2)
+    //               << p.first << ' ' << std::string(p.second*3, '*') << '\n';
+    // }
+    // printf("\n");
+
+    // printf("[Case_study] 4.5 m DATA\n");
+    // for(auto p : hist_measure_4_5) {
+    //     std::cout << std::setw(2)
+    //               << p.first << ' ' << p.second << '\n';
+    // }
+    // printf("\n");
+
+    printf("[Case_study] tof 0.2 m HISTOGRAM\n");
+    for(auto p : hist_measure_tof_0_2) {
+        std::cout << std::setw(2)
+                  << p.first << ' ' << std::string(p.second*3, '*') << '\n';
+    }
+    printf("\n");
+
+    printf("[Case_study] tof 0.2 mDATA\n");
+    for(auto p : hist_measure_tof_0_2) {
+        std::cout << std::setw(2)
+                  << p.first << ' ' << p.second << '\n';
+    }
+    printf("\n");
+}
--- case_study/scripts/curva_std_dev.py
+++ case_study/scripts/curva_std_dev.py
@@ -1,11 +1,33 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
-x = [0, 75, 150, 300, 450]
-y = [0, 14.28, 21.31, 25.68, 30.99]
+# Dati originali
+x_data = np.array([0, 661, 750, 1500, 3000, 4500])
+y_data = np.array([0, 0.18, 0.46, 0.69, 0.83, 1])
 
-plt.plot(x, y, marker='o')
-plt.xlabel('Distanza (cm)')
+# Calcolo dei coefficienti
+n = len(x_data)
+Σx = np.sum(x_data)
+Σy = np.sum(y_data)
+Σxy = np.sum(x_data * y_data)
+Σx2 = np.sum(x_data**2)
+
+m = (n * Σxy - Σx * Σy) / (n * Σx2 - Σx**2)
+b = (Σy - m * Σx) / n
+print('Coefficiente angolare: ', m)
+
+# Generazione dei punti sulla retta approssimata
+x_line = np.linspace(0, 5000, 100)
+y_line = m * x_line + b
+
+# Plot dei dati originali e della retta approssimata
+plt.scatter(x_data, y_data, color='blue', label='Dati originali')
+plt.plot(x_line, y_line, color='red', label='Retta approssimata')
+
+plt.xlabel('Distanza(mm)')
 plt.ylabel('Std_Dev')
-plt.title('Grafico della curva')
+plt.title('Approssimazione lineare')
+plt.legend()
 plt.grid(True)
 plt.show()
+
